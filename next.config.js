ea46b948f4d49156e00a005896acd5cc11d0b2c3
Per <38952136+persteenolsen@users.noreply.github.com>
2022-07-09 12:50:21 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,6 @@ module.exports = {
     publicRuntimeConfig: {
         apiUrl: process.env.NODE_ENV === 'development'
             ? 'http://localhost:3000/api' // development api
-            : 'http://localhost:3000/api' // production api
+            : 'https://psonextjsone.netlify.app/api' // production api
     }
 }
